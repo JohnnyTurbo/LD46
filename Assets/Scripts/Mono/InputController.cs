7a7a19ff48ba9163b3a1d73a334b58f820ca06d2
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-04-18 10:46:52 -0700

    Can click anywhere on the globe. A line renderer arc is drawn between where you click and the main LD server. Big arc when you click on the moon 😂😂

---
+++ Assets/Scripts/Mono/InputController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public GameObject LDMainServer;
+    public GameObject hitPrefab;
+    public int lineSegments;
+
+    private Camera mainCam;
+    private LineRenderer arc;
+
+    private void Start()
+    {
+        mainCam = Camera.main;
+        arc = GetComponent<LineRenderer>();
+        arc.positionCount = lineSegments;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                Instantiate(hitPrefab, hit.point, Quaternion.identity);
+                for(int i = 0; i < arc.positionCount; i++)
+                {
+                    //float slerpPct = 
+                    Vector3 arcPointPos = Vector3.Slerp(hit.point, LDMainServer.transform.position, i / (lineSegments - 1f));
+                    arc.SetPosition(i, arcPointPos);
+                }
+            }
+        }
+    }
+}
