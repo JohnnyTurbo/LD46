346170b918e8ccc611951d246d6f76183dfcef24
DESKTOP-I02B1D9\Johnny <jthompson59@mail.csuchico.edu>
2020-04-17 23:25:06 -0700

    Initial commit of Unity Project. Day 1 of dev complete. Earth and moon with placeholder materials in place. Basic camera movement. Goodnight.

---
+++ Assets/Scripts/Mono/CameraController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform earth;
+
+    public float horizontalPct, verticalPct;
+    public float minCamDist, maxCamDist;
+    public float rotSpeed;
+    public float zoomSpeed;
+
+    float leftZone;
+    float rightZone;
+    float botZone;
+    float topZone;
+
+    private void Start()
+    {
+        leftZone = horizontalPct * Screen.width;
+        rightZone = Screen.width - (horizontalPct * Screen.width);
+        botZone = verticalPct * Screen.height;
+        topZone = Screen.height - (verticalPct * Screen.height);
+    }
+
+    void Update()
+    {
+        if (Input.mousePosition.x <= leftZone)
+        {
+            //Debug.Log("Go Left " + leftZone);
+            transform.position = Quaternion.AngleAxis(rotSpeed * Time.deltaTime, Vector3.up) * (transform.position - earth.position) + earth.position;
+        }
+        else if (Input.mousePosition.x >= rightZone)
+        {
+            //Debug.Log("Go Right " + rightZone);
+            transform.position = Quaternion.AngleAxis(rotSpeed * Time.deltaTime, Vector3.down) * (transform.position - earth.position) + earth.position;
+        }
+
+        Vector3 camVertAxis = Vector3.Cross((earth.position - transform.position), Vector3.up);
+
+        if (Input.mousePosition.y <= botZone)
+        {
+            //Debug.Log("Go Bot " + botZone);
+            transform.position = Quaternion.AngleAxis(rotSpeed * Time.deltaTime, camVertAxis) * (transform.position - earth.position) + earth.position;
+        }
+        else if (Input.mousePosition.y >= topZone)
+        {
+            //Debug.Log("Go Top " + topZone);
+            transform.position = Quaternion.AngleAxis(rotSpeed * Time.deltaTime, camVertAxis * -1) * (transform.position - earth.position) + earth.position;
+        }
+
+        transform.LookAt(earth);
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            //Debug.Log("above zero");
+            Vector3 zoomInTarget = Vector3.ClampMagnitude(transform.position - earth.position, minCamDist);
+            transform.position = Vector3.MoveTowards(transform.position, zoomInTarget, Time.deltaTime * zoomSpeed);
+            if (Vector3.Distance(transform.position, earth.position) <= minCamDist)
+            {
+                transform.position = zoomInTarget;
+            }
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            //Debug.Log("sub zero");
+            Vector3 zoomOutTarget = Vector3.Normalize(transform.position - earth.position) * maxCamDist;
+            Debug.Log(zoomOutTarget.ToString());
+            transform.position = Vector3.MoveTowards(transform.position, zoomOutTarget, Time.deltaTime * zoomSpeed);
+            if (Vector3.Distance(transform.position, earth.position) >= maxCamDist)
+            {
+                //transform.position = zoomOutTarget;
+            }
+        }
+    }
+}
